5194f5aae73c9ecc548d3be71cef5fef0542da11
RaheeqMousa <raheeqmousa00@gmail.com>
2026-08-16 00:01:59 +0300

    fixed n+1 problem

--- src/main/java/com/example/UsersManagement/repository/UserRepository.java
+++ src/main/java/com/example/UsersManagement/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.UsersManagement.repository;
 
 import com.example.UsersManagement.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,9 +15,12 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = {"addresses"})
     Optional<User> findByPhoneNumberAndDeletedFalse(String phoneNumber);
 
+    @EntityGraph(attributePaths = {"addresses"})
     Optional<User> findByIdAndDeletedFalse(Long id);
 
+    @EntityGraph(attributePaths = {"addresses"})
     List<User> findByDeletedFalse();
 }
